cd0c956f69634ba7d560e15a8f4b4ed9a6313fab
Sai Ganesh Raju <sai.raju@alumni.fh-aachen.de>
2022-12-22 02:10:39 +0100

    added missing json for button and new state machine

---
+++ cog_rob/cog_rob_YCEQNC/Distance_and_yaw_BECISG/Distance_correction_MVNMCV/script.py
@@ -0,0 +1,53 @@
+import rclpy
+from rclpy.node import Node
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+from geometry_msgs.msg import Twist
+
+class Distance(Node):
+    def __init__(self):
+        super().__init__('orient')
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.pub = self.create_publisher(Twist,'/cmd_vel',10)
+        self.timer = self.create_timer(0.1,self.distance_cb)
+        self.translate_y=False
+        self.vel = Twist()
+    def distance_cb(self):
+        try:
+            self.tf_buffer.wait_for_transform_async('base_link','can5', rclpy.time.Time())
+            self.t = self.tf_buffer.lookup_transform('base_link','can5', rclpy.time.Time())
+            if not self.translate_y:
+                if self.t.transform.translation.y>0.0:
+                    if not 0.0<self.t.transform.translation.y<0.04:
+                        self.vel.linear.y = 0.3
+                        self.pub.publish(self.vel)
+                    else:
+                        self.vel.linear.y = 0.0
+                        self.pub.publish(self.vel)
+                        self.translate_y = True
+                else:
+                    if not 0.0<self.t.transform.translation.y<0.04:
+                        self.vel.linear.y = -0.3
+                        self.pub.publish(self.vel)
+                    else:
+                        self.vel.linear.y = 0.0
+                        self.pub.publish(self.vel)
+                        self.translate_y = True
+        except:
+            pass
+
+def execute(self, inputs, outputs, gvm):
+    try:
+        rclpy.init()
+    except:
+        pass
+    distance = Distance()
+    while not distance.translate_y:
+        rclpy.spin_once(distance)
+    if inputs["num_tries"]!=3:
+        outputs["num"]=inputs["num_tries"]+1
+        return -2
+    else:
+        outputs["num"]=0
+        return 0
---
+++ cog_rob/cog_rob_YCEQNC/Save_Position_FQEDNA/script.py
@@ -0,0 +1,116 @@
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
+from sensor_msgs.msg import JointState
+from moveit_msgs.msg import MotionPlanRequest
+from moveit_msgs.msg import JointConstraint
+from moveit_msgs.msg import Constraints
+from moveit_msgs.msg import PlanningOptions
+from moveit_msgs.action import MoveGroup
+from action_msgs.msg import GoalStatus
+
+from copy import deepcopy
+
+global_joint_states = None
+var = False
+class MoveGroupActionClient(Node):
+
+    def __init__(self):
+        super().__init__('moveit_plan_execute_python')
+
+        self.motion_plan_request = MotionPlanRequest()
+        self.motion_plan_request.workspace_parameters.header.stamp = self.get_clock().now().to_msg()
+        self.motion_plan_request.workspace_parameters.header.frame_id = 'base_link'
+        self.motion_plan_request.workspace_parameters.min_corner.x = -1.0
+        self.motion_plan_request.workspace_parameters.min_corner.y = -1.0
+        self.motion_plan_request.workspace_parameters.min_corner.z = -1.0
+        self.motion_plan_request.workspace_parameters.max_corner.x = 1.0
+        self.motion_plan_request.workspace_parameters.max_corner.y = 1.0
+        self.motion_plan_request.workspace_parameters.max_corner.z = 1.0
+        self.motion_plan_request.start_state.is_diff = True
+        self.var = False
+
+        jc = JointConstraint()
+        jc.tolerance_above = 0.0001
+        jc.tolerance_below = 0.0001
+        jc.weight = 1.0
+
+        joints = {}
+        joints['shoulder_pan_joint'] = -1.6391
+        joints['shoulder_lift_joint'] = -0.1366
+        joints['elbow_joint'] = -2.0147
+        joints['wrist_1_joint'] = -0.7512
+        joints['wrist_2_joint'] = 1.5708
+        joints['wrist_3_joint'] = 0.0
+
+        constraints = Constraints()
+        for (joint, angle) in joints.items():
+            jc.joint_name = joint
+            jc.position = angle
+            constraints.joint_constraints.append(deepcopy(jc))
+
+        self.motion_plan_request.goal_constraints.append(constraints)
+
+        self.motion_plan_request.pipeline_id = 'move_group'
+        self.motion_plan_request.group_name = 'spot_ur3e_arm'
+        self.motion_plan_request.num_planning_attempts = 10
+        self.motion_plan_request.allowed_planning_time = 5.0
+        self.motion_plan_request.max_velocity_scaling_factor = 0.1
+        self.motion_plan_request.max_acceleration_scaling_factor = 0.1
+        self.motion_plan_request.max_cartesian_speed = 0.0
+
+        self.planning_options = PlanningOptions()
+        self.planning_options.plan_only = False
+        self.planning_options.look_around = False
+        self.planning_options.look_around_attempts = 0
+        self.planning_options.max_safe_execution_cost = 0.
+        self.planning_options.replan = True
+        self.planning_options.replan_attempts = 10
+        self.planning_options.replan_delay = 0.1
+
+        self.create_subscription(JointState, '/joint_states', self.joint_states_cb, 1)
+        self._action_client = ActionClient(self, MoveGroup, '/move_action')
+
+    def joint_states_cb(self, joint_state):
+        global global_joint_states
+        global_joint_states = joint_state
+        self.joint_state = joint_state
+
+    def send_goal(self):
+        global var
+        self.motion_plan_request.start_state.joint_state = self.joint_state
+
+        goal_msg = MoveGroup.Goal()
+        goal_msg.request = self.motion_plan_request
+        goal_msg.planning_options = self.planning_options
+        while not self._action_client.wait_for_server(timeout_sec=2.0):
+            continue
+        self.send_goal_future = self._action_client.send_goal_async(goal_msg)
+        rclpy.spin_until_future_complete(self, self.send_goal_future)
+        goal_handle = self.send_goal_future.result()
+        if not goal_handle.accepted:
+            self.var = False
+            return
+        self.get_result_future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(self, self.get_result_future)
+        status = self.get_result_future.result().status
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            var = True
+        return
+
+def execute(self, inputs, outputs, gvm):
+    global var
+    try:
+        rclpy.init()
+    except:
+        self.logger.info(".")
+    action_client = MoveGroupActionClient()
+    # Get info from /joint_states
+    while global_joint_states is None:
+        rclpy.spin_once(action_client)
+    self.logger.info("Received")
+    action_client.send_goal()
+    if var:
+        return 0
+    else:
+        return -1
